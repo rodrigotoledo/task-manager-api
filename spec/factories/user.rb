ada115043c1582ea224244e1df6926e886d0dc8a
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2018-03-05 16:09:27 -0300

    testing users and should matchers

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
 	factory :user do
+		email "rodrigo@rtoledo.inf.br"
+		password "asdqwe123"
+		password_confirmation "asdqwe123"
 	end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { build(:user) }
+
+  it { is_expected.to validate_presence_of(:email) }  
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }  
+  it { is_expected.to validate_confirmation_of(:password) }  
+  it { is_expected.to allow_value("agc.rodrigo@gmail.com").for(:email) }  
 end
