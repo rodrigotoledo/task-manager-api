b1fc3970f5424a4686f17f402a3341d726420d05
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2018-03-05 21:39:14 -0300

    first test of requests

---
+++ spec/requests/api/v1/users_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'Users API', type: :request do
+	let!(:user) { create(:user) }
+	let(:user_id) { user.id }
+
+	describe "get users/:id" do
+		before do
+			headers = { "Accept" => "application/vnd.taskmanager.v1" }
+			get "/users/#{user_id}", {}, headers
+		end
+
+		context "when users exists" do
+			it "returns the user" do
+				user_response = JSON.parse(response.body)
+				expect(user_response['id']).to eq(user_id) 
+			end
+		end
+		
+	end
+	
+end
